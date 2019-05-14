92e0b840ef6f3435fede13d76379e3bf8312688f
blackyau <blackyau426@gmail.com>
2019-05-15 00:07:48 +0800

    数据抓取核心完成

--- get.py
+++ get.py
@@ -2,21 +2,43 @@ import hashlib
 import random
 import requests
 import time
-import json
 from setting import headers
-
 
 md5 = hashlib.md5()
 md5.update(str(random.random()).encode('utf-8'))
 random_id = str(md5.hexdigest())  # 32位随机ID
 now_time = str(int(time.time() * 1000))  # 时间戳
 random_num = str(int(random.random() * 1000000))  # 随机6位数
-random_all = random_id+'-'+now_time+'-'+random_num
+random_all = random_id + '-' + now_time + '-' + random_num
 random_v = str(random.random())[:9]  # 随机7位小数
 
 
-payload = {'pageSize': 90, 'cityId': 530, 'industry': 10100, 'workExperience': -1, 'education': -1, 'companyType': -1, 'employmentType': -1, 'jobWelfareTag': -1, 'kt': 3, '_v': random_v, 'x-zp-page-request-id': random_all}
-r = requests.get('https://fe-api.zhaopin.com/c/i/sou', headers=headers, params=payload)
-print(r.text)
-json_data = json.loads(r.text)
-print(json_data)
+def get_sou(random_v_in, random_request_id, page=1, page_size=90, city_id=489, industry=0, kt=3, kw="", **more_information):
+    payload = {'start': page, 'pageSize': page_size, 'cityId': city_id, 'industry': industry, 'workExperience': -1,
+               'education': -1, 'companyType': -1, 'employmentType': -1, 'jobWelfareTag': -1, 'kw': kw, 'kt': kt,
+               '_v': random_v_in, 'x-zp-page-request-id': random_request_id}
+
+    if more_information:  # 把关键字参数写入
+        for key in more_information.keys():
+            payload[key] = more_information[key]
+
+    if payload['start'] <= 1:  # 只获取第一页不需要带这个参数
+        payload.pop('start')
+    else:  # 计算页数
+        payload['start'] = (payload['start']-1)*payload['pageSize']
+    if payload['kw'] == '':  # 不要关键字就不带参数
+        payload.pop('kw')
+    if payload['industry'] == 0:  # 不要行业就不带参数
+        payload.pop('industry')
+
+    r = requests.get('https://fe-api.zhaopin.com/c/i/sou', headers=headers, params=payload)
+    print(random_v_in)
+    print(random_request_id)
+
+    if "\"results\":[]" in r.text:
+        raise Warning("返回数据异常!以下是 API 返回信息\n"+r.text)
+    else:
+        return r
+
+
+print(get_sou(random_v, random_all, 1).text)
