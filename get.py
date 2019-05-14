42716b8bed092c05c2d2f083d9474fe0d64abef3
blackyau <blackyau426@gmail.com>
2019-05-14 17:40:14 +0800

    params 初步测试完成

--- get.py
+++ get.py
@@ -1,5 +1,9 @@
-from setting import headers, cookies
-import requests, hashlib, random, time
+import hashlib
+import random
+import requests
+import time
+import json
+from setting import headers
 
 
 md5 = hashlib.md5()
@@ -7,8 +11,12 @@ md5.update(str(random.random()).encode('utf-8'))
 random_id = str(md5.hexdigest())  # 32位随机ID
 now_time = str(int(time.time() * 1000))  # 时间戳
 random_num = str(int(random.random() * 1000000))  # 随机6位数
+random_all = random_id+'-'+now_time+'-'+random_num
 random_v = str(random.random())[:9]  # 随机7位小数
 
-# r = requests.get('https://fe-api.zhaopin.com/c/i/sou', headers=headers, cookies=cookies)
-# print(r.url)
-# print(r.text)
+
+payload = {'pageSize': 90, 'cityId': 530, 'industry': 10100, 'workExperience': -1, 'education': -1, 'companyType': -1, 'employmentType': -1, 'jobWelfareTag': -1, 'kt': 3, '_v': random_v, 'x-zp-page-request-id': random_all}
+r = requests.get('https://fe-api.zhaopin.com/c/i/sou', headers=headers, params=payload)
+print(r.text)
+json_data = json.loads(r.text)
+print(json_data)
--- setting.py
+++ setting.py
@@ -1,5 +1,7 @@
 headers = {
-
+    'Accept': 'application/json, text/plain, */*',
+    'Origin': 'https://sou.zhaopin.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
 }
 
 cookies = {
