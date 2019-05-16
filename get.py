734fb2738df29117b105003054b30f767594e38f
blackyau <blackyau426@gmail.com>
2019-05-16 16:21:11 +0800

    大概完成了爬虫

--- get.py
+++ get.py
@@ -17,13 +17,21 @@ def random_info():
 
 
 def city_info(str_in):
-    payload = {"ipCity": str_in}
-    r = requests.get("https://fe-api.zhaopin.com/c/i/city-page/user-city", headers=headers, params=payload)
-    return r.json()["data"]["code"]
+    if type(str_in) != str:
+        raise TypeError("只能传入字符串,地区名的全称")
+    else:
+        if str_in == "全国":
+            return 489
+        else:
+            payload = {"ipCity": str_in}
+            r = requests.get("https://fe-api.zhaopin.com/c/i/city-page/user-city", headers=headers, params=payload)
+            return r.json()["data"]["code"]
 
 
-def sou(random_v_in, random_request_id, page=1, page_size=90, city_name="全国", industry=0, kt=3, kw="", **more_information):
-    payload = {'start': page, 'pageSize': page_size, 'cityId': city_info(city_name), 'industry': industry, 'workExperience': -1,
+def sou(random_v_in, random_request_id, page=1, page_size=90, city_name="全国", industry_value=0, kt=3, kw="",
+        **more_information):
+    payload = {'start': page, 'pageSize': page_size, 'cityId': city_info(city_name), 'industry': industry_value,
+               'workExperience': -1,
                'education': -1, 'companyType': -1, 'employmentType': -1, 'jobWelfareTag': -1, 'kw': kw, 'kt': kt,
                '_v': random_v_in, 'x-zp-page-request-id': random_request_id}
 
@@ -34,7 +42,7 @@ def sou(random_v_in, random_request_id, page=1, page_size=90, city_name="全国"
     if payload['start'] <= 1:  # 只获取第一页不需要带这个参数
         payload.pop('start')
     else:  # 计算页数
-        payload['start'] = (payload['start']-1)*payload['pageSize']
+        payload['start'] = (payload['start'] - 1) * payload['pageSize']
     if payload['kw'] == '':  # 不要关键字就不带参数
         payload.pop('kw')
     if payload['industry'] == 0:  # 不要行业就不带参数
@@ -43,24 +51,34 @@ def sou(random_v_in, random_request_id, page=1, page_size=90, city_name="全国"
     r = requests.get('https://fe-api.zhaopin.com/c/i/sou', headers=headers, params=payload)
 
     if "\"results\":[]" in r.text:
-        raise Warning("返回数据异常!以下是 API 返回信息\n"+r.text)
+        raise Warning("返回数据异常!以下是 API 返回信息\n" + r.text)
     else:
         return r
+
 
 # TODO extractTag extractSkillTag, 职位亮点 welfare, 工资 salary, workingExp workingExp["name"], jobName,eduLevel eduLevel["name"],
 # TODO city city["display"], companySum company["size"][""name""], companyType company["type"][""name""], jobType["items"]["name"](多个)
 
 
 if __name__ == "__main__":
-    with open("file.json", mode="w", encoding="utf-8") as file:
-        random_v, random_all = random_info()
-
-        for page_sum in range(1, 15):
-            try:
-                result = sou(random_v, random_all, page_sum, 90, "成都", 160400)
-                file.write(result.text)
-                print("Page", page_sum, "爬取完毕等待 3s 后爬取下一页")
-                time.sleep(3)
-            except Warning as Warning_info:
-                print("Page", page_sum, Warning_info)
-                break
+    start_time = time.time()
+    true_start_time = start_time
+    city_tup = ("全国", "北京", "上海", "广州", "深圳", "成都", "重庆")
+    industry_dict = {160400: "计算机软件", 160600: "网络游戏", 160000: "IT服务", 160200: "计算机硬件", 210500: "互联网_电子商务"}
+    for city in city_tup:
+        for industry in industry_dict.keys():
+            # TODO 判断是否创建路径
+            with open("./data/" + city + "_" + industry_dict[industry] + ".json", mode="w", encoding="utf-8") as file:
+                random_v, random_all = random_info()
+                for page_sum in range(1, 13):
+                    try:
+                        result = sou(random_v, random_all, page_sum, 90, city, industry)
+                        file.write(result.text)
+                        print("city:", city, " industry:", industry_dict[industry], " page:", page_sum, "爬取完毕 等待3s ")
+                        time.sleep(3)
+                        start_time = start_time - 3
+                    except Warning as Warning_info:
+                        print("Page", page_sum, Warning_info)
+                        break
+    end_time = time.time()
+    print("总共耗时:", end_time-true_start_time, "\n除去等待时间耗时:", end_time-start_time)
